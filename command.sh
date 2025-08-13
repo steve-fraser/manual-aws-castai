e721d129dc651735231e3ba2beb20fa1e712f7ad
Steve Fraser <steve@cast.ai>
2025-08-13 08:05:07 -0400

    adding commands

--- command.sh
+++ command.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+CREATE_AWS_NODE_ROLE=false CREATE_AWS_SECURITY_GROUP=false CREATE_AWS_POLICIES=false ./onboarding.sh
+SUBNET="<>" SG_GROUP="<>" INSTANCE_PROFILE="<>" CLUSTER_ID="<>"  ./set-node-role.sh
